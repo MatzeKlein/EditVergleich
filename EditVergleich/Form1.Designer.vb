0d78473894b76c677ea105f1fbb852d8db879889
Matthias Klein <klein@kwp-info.de>
2024-03-03 09:19:48 +0100

    Projektdateien hinzufügen.

---
+++ EditVergleich/Form1.Designer.vb
@@ -0,0 +1,66 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom)  _
+            Or System.Windows.Forms.AnchorStyles.Left)  _
+            Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.Location = New System.Drawing.Point(0, 27)
+        Me.TextBox1.Multiline = true
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox1.Size = New System.Drawing.Size(805, 425)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, -2)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(776, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Load c:\Tmp\BabylonIn.txt and write it to BabylonOut.txt"
+        Me.Button1.UseVisualStyleBackColor = true
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7!, 15!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(false)
+        Me.PerformLayout
+
+End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+End Class
---
+++ EditVergleich/Form1.vb
@@ -0,0 +1,54 @@
+﻿Imports System.IO
+Imports System.Text.RegularExpressions
+
+Public Class Form1
+    Const KateFilePath As String = "C:\Users\Klein\Documents\mk\Book Publishing\KateTmp.txt"
+    Const KateFilePath1 As String = "C:\Users\Klein\Documents\mk\Book Publishing\KateTmpGit.txt"
+    Const MattFilePath As String = "C:\Users\Klein\Documents\mk\Book Publishing\MattTmp.txt"
+    Const MattFilePath1 As String = "C:\Users\Klein\Documents\mk\Book Publishing\MattTmpGit.txt"
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        system.IO.File.Delete(MattFilePath1)
+        system.IO.File.Delete(KateFilePath1)
+        ConvertFile(MattFilePath, MattFilePath1)
+        ConvertFile(KateFilePath, KateFilePath1)
+    End Sub
+
+    Private Sub ConvertFile(filenamein As String, filenameOut As String)
+
+        Dim saetzeTMp As New List(Of String)
+        ' Read all lines from the file into an array
+        Dim lines As String() = File.ReadAllLines(filenamein)
+        For Each line As String In lines
+            dim orgline = line
+            line = line.Replace(".""", "§""")
+            line = line.Replace("?""", "§§""") '”
+            ' Define the regular expression pattern
+            Dim pattern As String = "([^.!?]+[.!?])"
+
+            ' Match the pattern in the input string
+            Dim matches As MatchCollection = Regex.Matches(line, pattern)
+            dim saetze() as String = Regex.Split(line, pattern)
+            If saetze.Count = 0 Then
+                saetzeTMp.Add(orgline)
+            Else
+                For Each lineTmp As String in saetze
+                    lineTmp = lineTmp.Replace("§§""", "?""")
+                    lineTmp = lineTmp.Replace("§""", ".""").Trim()
+                    lineTmp = lineTmp.Trim()
+                    if not String.IsNullOrWhiteSpace(lineTmp) Then
+                        saetzeTMp.Add(lineTmp)
+                    End If
+                Next
+            End If
+
+        Next
+
+        File.WriteAllLines(filenameOut, saetzeTMp)
+        TextBox1.Text = "Saetze Anzahl: " + saetzeTMp.Count.ToString() + ControlChars.CrLf + ControlChars.CrLf + String.Join(ControlChars.CrLf, saetzeTMp)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Button1.Text = "Process Files"
+    End Sub
+End Class
